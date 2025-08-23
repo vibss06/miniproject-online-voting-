b4a3b4764d9e72ed5532305e7a829a8af6bb5f84
Vibhanshu Gajalkar <vibhanshugajalkar006@gmail.com>
2025-08-23 13:09:53 +0530

    Create config.js

---
+++ frontend/config.js
@@ -0,0 +1,5 @@
+// LOCAL DEV: keep this
+// After backend is deployed on Render, replace with your Render URL.
+window.BACKEND_URL = "http://127.0.0.1:5000";
+// Example after deploy:
+// window.BACKEND_URL = "https://YOUR-BACKEND.onrender.com";
